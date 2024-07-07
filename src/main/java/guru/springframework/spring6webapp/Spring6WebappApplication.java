6241d3b19b6061948a0c85efff23ec4710e77485
Fady Zaafrane <fady.zaafrane@gmail.com>
2024-07-07 22:23:53 +0100

    first commit

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring6WebappApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello Fady");
+
         SpringApplication.run(Spring6WebappApplication.class, args);
     }
 
