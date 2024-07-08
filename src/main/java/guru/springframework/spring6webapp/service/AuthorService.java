d31cdde54d992cb2569ce52fa16d5ddfebeeeece
Fady Zaafrane <fady.zaafrane@gmail.com>
2024-07-08 10:44:39 +0100

    add add-authors-assn

---
+++ src/main/java/guru/springframework/spring6webapp/service/AuthorService.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6webapp.service;
+
+import guru.springframework.spring6webapp.domain.Author;
+import org.springframework.stereotype.Service;
+
+public interface AuthorService {
+    Iterable<Author> findAll();
+}
---
+++ src/main/java/guru/springframework/spring6webapp/service/AuthorServiceImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring6webapp.service;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Override
+    public Iterable<Author> findAll() {
+        return authorRepository.findAll();
+    }
+}
